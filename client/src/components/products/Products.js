55f2158b5017bcf1d056c85bd453189bf117622f
Travis Jette <travisjette@Traviss-MacBook-Pro.local>
2017-08-04 18:41:29 -0600

    now rendering products in products container

--- client/src/components/products/Products.js
+++ client/src/components/products/Products.js
@@ -3,7 +3,17 @@ import React from 'react';
 const Products = (props) => {
   return(
     <div>
-      <h1>{props.title}</h1>
+        {
+          props.productsData.map((product) => {
+            return(
+              <div className="productsCard">
+                <h3>{product.productName}</h3>
+                <p> {product.price}</p>
+                <p> {product.description} </p>
+              </div>
+            )
+          })
+        }
     </div>
   )
 }
--- client/src/containers/products/ProductsContainer.js
+++ client/src/containers/products/ProductsContainer.js
@@ -1,27 +1,40 @@
 import React from 'react';
 import {Products} from '../../components'
-
+import faker from 'faker';
 class ProductsContainer extends React.Component {
 
   state = {
-    title: undefined,
-    name: undefined,
+    productName: undefined,
     price: undefined,
-    quantity: undefined,
-    description: undefined
+    description: undefined,
+    products: undefined
   }
 
   componentDidMount(){
-    this.getTitle();
+    this.getProducts();
   }
 
-  getTitle = () => {
-    this.setState({title: "Products Catalog" })
+  getProducts = () => {
+    const productsArray = [];
+
+    for(var i=0; i<20; i++) {
+      productsArray.push({
+        productName: faker.commerce.productName(),
+        price: faker.commerce.price(),
+        description: faker.lorem.sentence(),
+      })
+    }
+    this.setState({products:productsArray})
   }
   render(){
     return (
       <div>
-      <Products title={this.state.title}  />
+      {
+        this.state.products
+        ? <Products productsData={this.state.products}  />
+        : <h1>Products being loaded</h1>
+      }
+
       </div>
     )
   }
