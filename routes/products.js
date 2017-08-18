6a5279e349f2ad51c393b5416943704fe491c0f5
Travis Jette <travisjette@Traviss-MacBook-Pro.local>
2017-08-18 13:18:43 -0600

    did tdd

--- routes/products.js
+++ routes/products.js
@@ -12,5 +12,40 @@ Router.route('/')
       }
     })
   })
+  .post((req, res) => {
+    const product = new Product({
+      name: req.body.name,
+      price: req.body.price,
+      image: req.body.image
+    })
+
+    product.save((err, product) => {
+      if (err) {
+        res.json({ message: err, data: null })
+      } else {
+        res.json({ message: "successfully posted product", data: product })
+      }
+    })
+  })
+
+Router.route('/:product_id')
+  .get((req,res) => {
+    Product.findById(req.params.product_id, (err,product) => {
+      if(err) {
+        res.json({ message: err, data: null })
+      } else {
+        res.json({ message: "received product by id", data: product })
+      }
+    })
+  })
+  .delete((req,res) => {
+    Product.remove({_id: req.params.product_id}, (err) => {
+      if (err) {
+        res.json({ message: err, data: null })
+      } else {
+        res.json({ message: "product deleted", data: {}})
+      }
+    })
+  })
 
 module.exports = Router
--- test/product.test.js
+++ test/product.test.js
@@ -28,4 +28,84 @@ describe('Products', () => {
       })
     })
   })
+
+  describe('/POST product', () => {
+    it('it should not post products without all fields', done => {
+    const product = { name: 'Computer' }
+    chai.request(server)
+      .post('/api/products')
+      .send(product)
+      .end((err,res) => {
+        res.should.have.status(200)
+        res.body.should.be.a('object')
+        res.body.message.should.have.property('errors')
+        res.body.should.have.property('data')
+        res.body.should.have.property('data').eql(null)
+        done()
+      })
+    })
+
+  it('it should POST a product with all fields', done => {
+    const product = new Product({
+      name: "hey",
+      price: 5.00,
+      image: 'n/a'
+    })
+    chai.request(server)
+      .post('/api/products')
+      .send(product)
+      .end((err,res) => {
+        res.should.have.status(200)
+        res.body.should.be.a('object')
+        res.body.message.length.should.be.above(0)
+        res.body.should.have.property('data')
+        res.body.should.be.a('object')
+        done()
+      })
+      })
+  })
+
+  describe('/GET/:product_id', () => {
+    it('should GET one product with all the fields', done => {
+      const product = new Product({
+        name: "an item",
+        price: 100,
+        image: 'n/a'
+      })
+      product.save((err,product) => {
+        chai.request(server)
+          .get(`/api/products/${product._id}`)
+          .end((err,res) => {
+            res.should.have.status(200)
+            res.body.should.be.a('object')
+            res.body.message.length.should.be.above(0)
+            res.body.should.have.property('data')
+            res.body.should.be.a('object')
+            done()
+          })
+      })
+    })
+  })
+
+  describe('/DELETE/:product_id product', () => {
+    it('it should DELETE a product', done => {
+      const product = new Product({
+        name: 'bike',
+        price: 9000,
+        image: 'n/a'
+      })
+      product.save((err, product) => {
+        chai.request(server)
+        .delete(`/api/products/${product._id}`)
+        .end((err,res) => {
+          res.should.have.status(200)
+          res.body.should.be.a('object')
+          res.body.message.length.should.be.above(0)
+          res.body.should.have.property('data')
+          res.body.data.should.eql({})
+          done()
+        })
+      })
+    })
+  })
 })
